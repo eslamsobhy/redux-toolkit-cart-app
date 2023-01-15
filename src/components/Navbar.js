50760cd6a9cd59aa3c9f1f23fd78ca6ac471e660
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-01-15 16:33:32 +0200

    cleaned things up

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import { CartIcon, Graduated } from "../icons";
+import { CartIcon } from "../icons";
 import { useSelector } from "react-redux";
 
 import React from "react";
@@ -7,20 +7,17 @@ const Navbar = () => {
   const { amount } = useSelector((store) => store.cart);
 
   return (
-    <div className="graduated">
-      <Graduated />
-      <nav>
-        <div className="nav-center">
-          <h3>redux toolkit</h3>
-          <div className="nav-container">
-            <CartIcon />
-            <div className="amount-container">
-              <p className="total-amount">{amount}</p>
-            </div>
+    <nav>
+      <div className="nav-center">
+        <h3>redux toolkit</h3>
+        <div className="nav-container">
+          <CartIcon />
+          <div className="amount-container">
+            <p className="total-amount">{amount}</p>
           </div>
         </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
   );
 };
 
