a8f518d7c6c70dcbc319b4c641ed112a172e0d72
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-01-15 16:32:13 +0200

    tested hero-icons as components and applyed styles for them

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import { CartIcon } from "../icons";
+import { CartIcon, Graduated } from "../icons";
 import { useSelector } from "react-redux";
 
 import React from "react";
@@ -7,17 +7,20 @@ const Navbar = () => {
   const { amount } = useSelector((store) => store.cart);
 
   return (
-    <nav>
-      <div className="nav-center">
-        <h3>redux toolkit</h3>
-        <div className="nav-container">
-          <CartIcon />
-          <div className="amount-container">
-            <p className="total-amount">{amount}</p>
+    <div className="graduated">
+      <Graduated />
+      <nav>
+        <div className="nav-center">
+          <h3>redux toolkit</h3>
+          <div className="nav-container">
+            <CartIcon />
+            <div className="amount-container">
+              <p className="total-amount">{amount}</p>
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 };
 
